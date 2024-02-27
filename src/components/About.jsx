1d66d3d51a06105b7f0c0a9a655117ceb3bc3029
Aniket Dalvi <aniketdalvi385@gmail.com>
2024-02-27 09:31:00 +0530

    initial site with dummy data.

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name="About" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, error! Ullam voluptates, perferendis itaque rerum ipsa explicabo, autem repellat dolore praesentium molestiae pariatur nulla sit laboriosam et veritatis sequi aspernatur dicta natus illo a quaerat nisi inventore quo. Repellendus aliquam natus nesciunt quasi sapiente voluptates provident corrupti reprehenderit porro sequi?</p>
+                <br />
+                <p className='text-xl'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio nam consectetur accusantium cum voluptates necessitatibus numquam similique, atque possimus fugiat voluptatum asperiores iure reprehenderit ea eveniet, nobis at molestiae recusandae nesciunt mollitia molestias cupiditate quos obcaecati beatae. Molestias, explicabo qui dolorem voluptatum officia temporibus id veritatis repudiandae cum. Voluptate, consectetur?</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
