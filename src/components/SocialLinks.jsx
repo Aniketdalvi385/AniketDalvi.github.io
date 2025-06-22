4eac137b06676e21d5119c34cd504fb1d5aeb644
Aniket Dalvi <166008738+Aniket-AGR@users.noreply.github.com>
2025-06-22 17:19:25 +0530

    Update SocialLinks.jsx to new resume link

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
 					Resume <BsFillPersonLinesFill size={30} />
 				</>
 			),
-			href: "https://drive.google.com/file/d/1rQV-7Miq7ALdDKOCS67ddfgysTiFsnxZ/view?usp=sharing",
+			href: "https://drive.google.com/file/d/1eZQEMr4Ctt4dBgY44EIluwPHz9zBTce1/view?usp=drive_link",
 			style: "rounded-br-md",
 			download: true,
 		},
